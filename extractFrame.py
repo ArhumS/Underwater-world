93506412219649ec5a7d112e2711c932929d0b21
ArhumS <35311331+ArhumS@users.noreply.github.com>
2018-08-16 10:51:56 -0400

    shell script
    shell script to pull apart a video and make frames

---
+++ extractFrame.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Extract frames from a stereo video file
+#
+import sys
+import os
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print "usage: extractFrames.py video vid-dir"
+  else:
+    if os.path.exists(sys.argv[2]):
+      print "Output directory " + sys.argv[2] + " already exists"
+      sys.exit(1)
+    os.mkdir(sys.argv[2])
+    os.system("./ffmpeg -i " + sys.argv[1] + " -map 0:0 -vcodec copy -an " + sys.argv[2] + "/left.avi")
+    os.system("./ffmpeg -i " + sys.argv[1] + " -map 0:2 -vcodec copy -an " + sys.argv[2] + "/right.avi")
+    os.system("./ffmpeg -i " + sys.argv[2] + "/left.avi -an " + sys.argv[2] + "/left.%d.ppm")
+    os.system("./ffmpeg -i " + sys.argv[2] + "/right.avi -an " + sys.argv[2] + "/right.%d.ppm")
