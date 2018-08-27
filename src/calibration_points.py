4ee7dcc05f4e65c66b3ab3ef92ab32f10e7a6b4d
Michael Jenkin <jenkin@eecs.yorku.ca>
2018-08-27 09:44:39 -0400

    fix to display

--- src/calibration_points.py
+++ src/calibration_points.py
@@ -7,6 +7,7 @@
 # Version History
 #   v1.0 - preliminary version
 #   V2.0 - fancier graphics and now outputs the image shape in the json
+#   v2.1 - even fancier graphics
 #
 
 import glob
@@ -16,16 +17,19 @@ import json
 import numpy as np
 import argparse
 
-# each image has this size calibration grid. 
-rows=7
-columns=6
+# Default size of calibration grid is 7x6
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filepath', help='String directory')
 parser.add_argument('output', help='String output filename')
 parser.add_argument('--display', type=bool, default=False, help='display checkerboards as processed')
+parser.add_argument('--rows', type=int, default=7, help='number of rows in checkerboard')
+parser.add_argument('--cols', type=int, default=6, help='number of cols in checkerboard')
 args = parser.parse_args()
+rows = args.rows
+columns = args.cols
+print rows, columns
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20,0.001)
 objp = np.zeros((columns*rows,3), np.float32)
@@ -53,66 +57,48 @@ for i in range(0 , lengthr) :
   imgl = cv2.imread(limages[i])
   imgl[:,:,0] = imgl[:,:,2]
   grayl = cv2.cvtColor(imgl,cv2.COLOR_BGR2GRAY)
-  ret, cornersl = cv2.findChessboardCorners(grayl, (rows,columns), None)
-  if not ret :
-    print "left point finder fails"
-    if args.display :
-      tmpl = cv2.resize(imgl ,(0,0) ,None ,0.5, 0.5)
-      cv2.putText(tmpl, limages[i] + ' fails', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-      cv2.imshow('left', tmpl)
-      cv2.waitKey(250)
-    continue
-  cv2.cornerSubPix(grayl,cornersl,(11,11),(-1,-1),criteria)
-  if args.display :
-    cv2.drawChessboardCorners(imgl,(rows,columns), cornersl, ret)
-    tmpl = cv2.resize(imgl ,(0,0) ,None ,0.5, 0.5)
+  retl, cornersl = cv2.findChessboardCorners(grayl, (rows,columns), None)
+  tmpl = cv2.resize(imgl ,(0,0) ,None ,0.5, 0.5)
+  if not retl :
+    cv2.putText(tmpl, limages[i] + ' fails', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+  else :
+    cv2.cornerSubPix(grayl, cornersl, (11,11), (-1,-1), criteria)
     cv2.putText(tmpl, limages[i] + ' success', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-    cv2.imshow('left', tmpl)
-    cv2.waitKey(250)
 
   imgr = cv2.imread(rimages[i])
   imgr[:,:,0] = imgr[:,:,2]
   grayr = cv2.cvtColor(imgr,cv2.COLOR_BGR2GRAY)
-  ret, cornersr = cv2.findChessboardCorners(grayr, (rows,columns), None)
-  print ret
-  if not ret :
-    print "right point finder fails"
-    if args.display :
-      tmpr = cv2.resize(imgr ,(0,0) ,None ,0.5, 0.5)
-      cv2.putText(tmpr, rimages[i] + ' fails', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-      cv2.imshow('right', tmpr)
-      cv2.waitKey(250)
-    continue
-  cv2.cornerSubPix(grayr,cornersr,(11,11),(-1,-1),criteria)
-  if args.display :
-    cv2.drawChessboardCorners(imgr,(rows,columns), cornersl, ret)
-    tmpr = cv2.resize(imgr ,(0,0) ,None ,0.5, 0.5)
+  retr, cornersr = cv2.findChessboardCorners(grayr, (rows,columns), None)
+  tmpr = cv2.resize(imgr ,(0,0) ,None ,0.5, 0.5)
+  if not retr :
+    cv2.putText(tmpr, rimages[i] + ' fails', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+  else :
+    cv2.cornerSubPix(grayr, cornersr, (11,11), (-1,-1), criteria)
     cv2.putText(tmpr, rimages[i] + ' success', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-    cv2.imshow('right', tmpr)
+
+  if args.display :
+    nh = np.array(np.hstack((tmpl, tmpr)))
+    cv2.imshow('pair', nh)
     cv2.waitKey(250)
  
-  print "image pair good"
 
-  pts = cornersl
-  vl = []
-  for z in pts :
-    vl.append(str(z[0][0]))
-    vl.append(str(z[0][1]))
-  pts = cornersr
-  vr = []
-  for z in pts :
-    vr.append(str(z[0][0]))
-    vr.append(str(z[0][1]))
+  if retl and retr :
+    print "image pair good"
+    pts = cornersl
+    vl = []
+    for z in pts :
+      vl.append(str(z[0][0]))
+      vl.append(str(z[0][1]))
+    pts = cornersr
+    vr = []
+    for z in pts :
+      vr.append(str(z[0][0]))
+      vr.append(str(z[0][1]))
   
-  pair = {'left' : limages[I], 'right' : rimages[I], 'leftpts' : vl, 'rightpts' : vr, 'leftshape' : imgl.shape, 'rightshape' : imgr.shape}
-  goodset['pair' +  str(count)] = pair
-  count = count + 1
+    pair = {'left' : limages[i], 'right' : rimages[i], 'leftpts' : vl, 'rightpts' : vr, 'leftshape' : imgl.shape, 'rightshape' : imgr.shape}
+    goodset['pair' +  str(count)] = pair
+    count = count + 1
 
-  cv2.drawChessboardCorners(imgl,(rows,columns), cornersl, ret)
-  cv2.drawChessboardCorners(imgr,(rows,columns), cornersr, ret)
-  tmpl = cv2.resize(imgl ,(0,0) ,None ,0.5, 0.5)
-  tmpr = cv2.resize(imgr ,(0,0) ,None ,0.5, 0.5)
- 
  
 with open(args.output,"w") as outfile :
    json.dump(goodset, outfile)
