af352a552c5148299ac7bb2a89c14eee7364d2da
ArhumS <35311331+ArhumS@users.noreply.github.com>
2020-10-14 15:43:25 -0400

    Extended Version - with rectification add on
    Mean Reprojection Error:  0.08832608128910893
    img_size (1280, 720)
    Intrinsic_mtx_1 [[1.47449820e+04 0.00000000e+00 3.78582325e+02]
     [0.00000000e+00 1.36362127e+04 3.29276486e+02]
     [0.00000000e+00 0.00000000e+00 1.00000000e+00]]
    dist_1 [[-8.70791356e-01 -4.33909887e+03  3.71478956e-01  2.94427444e-02
       6.86098266e+01  0.00000000e+00  0.00000000e+00  0.00000000e+00
       0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00
       0.00000000e+00  0.00000000e+00]]
    Intrinsic_mtx_2 [[1.30399079e+04 0.00000000e+00 6.35442561e+02]
     [0.00000000e+00 1.14506760e+04 3.64825964e+02]
     [0.00000000e+00 0.00000000e+00 1.00000000e+00]]
    dist_2 [[-1.08532809e+01  5.38129000e+03  2.67009670e-01 -1.24548515e-02
       1.06082556e+01  0.00000000e+00  0.00000000e+00  0.00000000e+00
       0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00
       0.00000000e+00  0.00000000e+00]]
    R [[ 0.99989836  0.00367345 -0.01377566]
     [-0.00371582  0.99998844 -0.00305185]
     [ 0.01376429  0.00310272  0.99990045]]
    T [[ -3.08356568]
     [  0.05837594]
     [-39.22240271]]
    E [[-1.44939972e-01  3.92221304e+01 -6.13305764e-02]
     [-3.91759732e+01 -1.34513922e-01  3.62357340e+00]
     [-4.69120209e-02 -3.08374447e+00  1.02147324e-02]]
    F [[ 5.90920930e-08 -1.72911095e-05  6.03987551e-03]
     [ 1.81888144e-05  6.75308649e-08 -3.17147235e-02]
     [-6.42389940e-03  2.86902512e-02  1.00000000e+00]]
    R1 [[ 0.09209503  0.00189735  0.99574842]
     [-0.00205484  0.99999642 -0.00171539]
     [-0.9957481  -0.00188813  0.09209859]]
    Q [[ 1.00000000e+00  0.00000000e+00  0.00000000e+00  3.57281853e+05]
     [ 0.00000000e+00  1.00000000e+00  0.00000000e+00 -3.62740250e+03]
     [ 0.00000000e+00  0.00000000e+00  0.00000000e+00  1.25434443e+04]
     [ 0.00000000e+00  0.00000000e+00  2.54171783e-02 -0.00000000e+00]]
    ROI1 (0, 0, 0, 0)
    ROI2 (0, 533, 1280, 187)

--- src/computeCalibration.py
+++ src/computeCalibration.py
@@ -4,84 +4,124 @@ import cv2
 import numpy as np
 import argparse
 
-class computeCalibration(object) :
-  def __init__(self, filepath) :
 
-  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-  objp = np.zeros((6*7,3), np.float32)
-  objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-  objp = objp.reshape(-1,1,3)
-  axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-  
-  objpoints = []
-  imgpoints = []
-  
-  print "Computing calibration information from " + filepath
-  limages=sorted(glob.glob(filepath + '/left.*.ppm'))
-  rimages=sorted(glob.glob(filepath + '/right.*.ppm'))
-  
-  length_l=len(limages)
-  length_r=len(rimages)
-  
-  print 'there are ' + str(length_l) + ' left images '
-  print 'there are ' + str(length_r) + ' right images '
-  
-  needed = 10
-  good=[]
-  while len(good) < needed:
-    x=random.randint(0,length_l-1)
-    if x in good:
-      continue
-    print 'trying ' + limages[x]
-    img_l = cv2.imread(limages[x])
-    img_l[:,:,0] = img_l[:,:,2] # blue from red (why?)
-    gray = cv2.cvtColor(img_l,cv2.COLOR_BGR2GRAY)
-    ret, corners_l = cv2.findChessboardCorners(gray, (7,6), None)
-    print corners_l
-    if not ret :
-      continue
-  
-    print 'trying ' + rimages[x]
-    img_r = cv2.imread(rimages[x])
-    img_r[:,:,0] = img_r[:,:,2] # blue from red (why?)
-    gray = cv2.cvtColor(img_r,cv2.COLOR_BGR2GRAY)
-    ret, corners_r = cv2.findChessboardCorners(gray, (7,6), None)
-    print corners_r
-    if not ret :
-      continue
-    print 'The Pair ' + limages[x] + " " +rimages[x] + " has the right number of calibration pts "
-    good.append(x)
-    print good
-    print len(good)
-  if  ret == True:
-    objpoints.append(objp)
-    cv2.cornerSubPix(gray,corners_l,(11,11),(-1,-1),criteria)
-    imgpoints.append(corners_l)
-  
-    cv2.drawChessboardCorners(img_l, (7,6), corners_l, ret)
-    cv2.drawChessboardCorners(img_r, (7,6), corners_r, ret)
-    imageln = cv2.resize(img_l, (0,0), None, 0.45, 0.45)
-    imagern = cv2.resize(img_r, (0,0), None, 0.45, 0.45)
-    numpy_vert = np.vstack((imageln, imagern))
-    cv2.imshow('Numpy Horizontal Concat', numpy_vert)
-    cv2.waitKey(5000)
-  
-  ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints , gray.shape[::-1], None, None)
-  print "Camera matrix",mtx
-  print "Distortion co-efficient", dist
-  print "Rotatioanl vectors",rvecs
-  print "Translational vector",tvecs
-  
-  tot_error = 0
-  for i in xrange(len(objpoints)):
-  	imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-  	error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-  	tot_error += error
-  print "Mean Reprojection Error: ", tot_error/len(objpoints)
-  
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', help='String directory')
-    args = parser.parse_args()
-    computeCalibration(args.filepath)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((7*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:7].T.reshape(-1,2)
+objp = objp.reshape(-1,1,3)
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
+objpoints = []
+imgpoints_l = []
+imgpoints_r = []
+  
+print("Computing calibration information from ")
+limages=sorted(glob.glob('left*.jpg'))
+rimages=sorted(glob.glob('right*.jpg'))
+  
+length_l=len(limages)
+length_r=len(rimages)
+  
+print('there are ' + str(length_l) + ' left images ')
+print('there are ' + str(length_r) + ' right images ')
+  
+needed = 20
+good=[]
+while len(good) < needed:
+   x=random.randint(0,length_l-1)
+   if x in good:
+       continue
+   print('trying ' + limages[x])
+   img_l = cv2.imread(limages[x])
+   img_l[:,:,0] = img_l[:,:,2] # blue from red (why?)
+   gray_l= cv2.cvtColor(img_l,cv2.COLOR_BGR2GRAY)
+   ret, corners_l = cv2.findChessboardCorners(gray_l, (9,7), None)
+   print(corners_l)
+   if not ret :
+       continue
+   
+   print('trying ' + rimages[x])
+   img_r = cv2.imread(rimages[x])
+   img_r[:,:,0] = img_r[:,:,2] # blue from red (why?)
+   gray_r = cv2.cvtColor(img_r,cv2.COLOR_BGR2GRAY)
+   ret, corners_r = cv2.findChessboardCorners(gray_r, (9,7), None)
+   print(corners_r)
+   if not ret :
+       continue
+   print('The Pair ' + limages[x] + " " +rimages[x] + " has the right number of calibration pts ")
+   good.append(x)
+   print(good)
+   print(len(good))
+   if  ret == True:
+     objpoints.append(objp)
+     cv2.cornerSubPix(gray_l,corners_l,(11,11),(-1,-1),criteria)
+     imgpoints_l.append(corners_l)
+     cv2.cornerSubPix(gray_r,corners_r,(11,11),(-1,-1),criteria)
+     imgpoints_r.append(corners_r)
+  
+     cv2.drawChessboardCorners(img_l, (9,7), corners_l, ret)
+     cv2.drawChessboardCorners(img_r, (9,7), corners_r, ret)
+     
+     imageln = cv2.resize(img_l, (0,0), None, 0.45, 0.45)
+     imagern = cv2.resize(img_r, (0,0), None, 0.45, 0.45)
+     numpy_vert = np.vstack((imageln, imagern))
+     cv2.imshow('Good Pairs ', numpy_vert)
+     cv2.waitKey(1000)
+     img_shape = gray_l.shape[::-1]
+
+   ret, mtxl, distl, rvecsl, tvecsl = cv2.calibrateCamera(objpoints, imgpoints_l , gray_l.shape[::-1], None, None)
+   ret, mtxr, distr, rvecsr, tvecsr = cv2.calibrateCamera(objpoints, imgpoints_r , gray_r.shape[::-1], None, None)
+
+   print("Left Camera matrix",mtxl)
+   print("Left Camera distortion co-efficient", distl)
+   print("Left Camera Rotatioanl vectors",rvecsl)
+   print("Left Camera Translational vector",tvecsl)
+
+   print("Right Camera matrix",mtxr)
+   print("Right Camera distortion co-efficient", distr)
+   print("Right Camera Rotatioanl vectors",rvecsr)
+   print("Right Camera Translational vector",tvecsr)
+ 
+   tot_error = 0
+   print("number of points",len(objpoints))
+   for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecsl[i], tvecsl[i], mtxl, distl)
+    error = cv2.norm(imgpoints_l[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    tot_error += error
+   print("Mean Reprojection Error: ", tot_error/len(objpoints))
+
+   #def stereo_calibrate(self, dims):
+ 
+   flags = 0
+   flags |= cv2.CALIB_FIX_INTRINSIC
+   flags = CALIB_ZERO_DISPARITY
+   # flags |= cv2.CALIB_FIX_PRINCIPAL_POINT
+   flags |= cv2.CALIB_USE_INTRINSIC_GUESS
+   flags |= cv2.CALIB_FIX_FOCAL_LENGTH
+   # flags |= cv2.CALIB_FIX_ASPECT_RATIO
+   flags |= cv2.CALIB_ZERO_TANGENT_DIST
+   flags |= cv2.CALIB_RATIONAL_MODEL
+   
+   img_shape = gray_l.shape[::-1]
+   print("img_size",img_shape)
+   stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER +
+                                cv2.TERM_CRITERIA_EPS, 100, 1e-5)
+   ret, mtxl, distl, mtxr,distr, R, T, E, F = cv2.stereoCalibrate(objpoints, imgpoints_l,
+                imgpoints_r, mtxl,distl, mtxr,distr,img_shape, flags=flags,criteria=stereocalib_criteria)
+   print('Intrinsic_mtx_1', mtxl)
+   print('dist_1', distl)
+   print('Intrinsic_mtx_2', mtxr)
+   print('dist_2', distr)
+   print('R', R)
+   print('T', T)
+   print('E', E)
+   print('F', F)
+   
+   R1, R2, P1, P2, Q, ROI1, ROI2 = cv2.stereoRectify(mtxl,distl, mtxr, distr, img_shape, R,T,flag=flags)
+   print('R1', R1)
+   print('Q', Q)
+   print('ROI1', ROI1)
+   print('ROI2', ROI2)
+   
+
+   
